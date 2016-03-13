b3be55f157a2077384369de60854bc419f04f6bd
Kazhord <perso@kazhord.fr>
2016-03-13 19:39:36 +0100

    Update 2016-03-13T18:39:34.649Z

--- test/e2e/demo.e2e.js
+++
@@ -1,6 +0,0 @@
-describe('Protractor Demo', function() {
-	it('should have a title', function() {
-		browser.get('http://localhost:3000/');
-		expect(browser.getTitle()).toEqual('Starter Template for Bootstrap');
-	});
-});
--- test/unit/demo.spec.js
+++
@@ -1,7 +0,0 @@
-describe('unit: Demo', function () {
-
-    it('should be always true', function () {
-        expect(true).toEqual(true);
-    });
-
-});
